a8c46c096e278e59cbd1c0b23d9c594a4190f848
ToddLynn <810304762@qq.com>
2022-07-15 14:29:57 +0800

    Git日志：备份-<not done>structured_output_v1.0.py: 对模型输出的dict中的spo_list,获取其中head/tail entity的准确idx;【done】5元素dict ->7元素dict 2022年7月15日-14:30-周五

--- structured_output_v1.0.py
+++ structured_output_v1.0.py
@@ -131,56 +131,32 @@ def get_entity_index(output_dict):
 
     # 3.开始获取entity的index
 
+    li_spo_with_idx = []
     for spo in li_spo:
-        # for
-        tail = spo["object"]["@value"]  #尾实体的具体文本
-        head = spo["subject"]           #头实体的具体文本
+        tail = spo["object"]["@value"]  # 尾实体的具体文本
+        head = spo["subject"]  # 头实体的具体文本
+        label_tail = spo["object_type"]["@value"]  # 尾实体的标签
+        label_head = spo["subject_type"]  # 头实体的标签
+        predicate = spo["predicate"]
 
         for sentence in li_sentence:
             idx_sentence = text.find(sentence)
             if tail in sentence and head in sentence:
                 idx_tail = [text.find(tail, idx_sentence), text.find(tail, idx_sentence) + len(tail)]
                 idx_head = [text.find(head, idx_sentence), text.find(head, idx_sentence) + len(head)]
-                d_new_idx ={
-                    "subject":head,
-                    "subject_idx":idx_head,
-                    "object":tail,
-                    "object_idx":idx_tail,
+                spo_with_idx = {
+                    "subject": head,
+                    "subject_idx": idx_head,
+                    "subject_type": label_head,
+                    "object": tail,
+                    "object_idx": idx_tail,
+                    "object_type": label_tail,
+                    "predicate": predicate
                 }
-                print(d_new_idx)
-"""
-子宫大小在正常范围,边缘规则,子宫肌层回声均匀;宫腔线居中,子宫内膜厚度为12mm。左侧附件见25×34mm无回声,边界清晰,囊壁平滑。右侧附件未见明显异常。CDFI:未见异常血流信号。
+                # print(d_new_idx)
+                li_spo_with_idx.append(spo_with_idx)
 
-{'subject': '子宫', 'subject_idx': [0, 2], 'object': '在正常范围', 'object_idx': [4, 9]}
-{'subject': '子宫', 'subject_idx': [0, 2], 'object': '规则', 'object_idx': [12, 14]}
-{'subject': '子宫内膜', 'subject_idx': [30, 34], 'object': '12mm', 'object_idx': [37, 41]}
-{'subject': 'CDFI', 'subject_idx': [79, 83], 'object': '未见异常血流信号', 'object_idx': [84, 92]}
-{'subject': '附件', 'subject_idx': [70, 72], 'object': '右', 'object_idx': [68, 69]}
-{'subject': '附件', 'subject_idx': [44, 46], 'object': '左', 'object_idx': [42, 43]}
-{'subject': '附件', 'subject_idx': [70, 72], 'object': '未见明显异常', 'object_idx': [72, 78]}
-{'subject': '附件', 'subject_idx': [44, 46], 'object': '25×34mm', 'object_idx': [47, 54]}
-{'subject': '附件', 'subject_idx': [44, 46], 'object': '无回声', 'object_idx': [54, 57]}
-{'subject': '附件', 'subject_idx': [44, 46], 'object': '清晰', 'object_idx': [60, 62]}
-{'subject': '附件', 'subject_idx': [44, 46], 'object': '平滑', 'object_idx': [65, 67]}
-{'subject': '子宫肌层', 'subject_idx': [15, 19], 'object': '均匀', 'object_idx': [21, 23]}
-
-Process finished with exit code 0
-"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return li_spo_with_idx
 
 
 if __name__ == '__main__':
@@ -211,5 +187,207 @@ if __name__ == '__main__':
                           "object": {"@value": "均匀"}, "subject": "子宫肌层"}]}
 
     # d = structured_output(dict)
-    d = get_entity_index(dict)
-    print(d)
+    li = get_entity_index(dict)
+    print(li)
+# [
+#   {
+#     "subject": "子宫",
+#     "subject_idx": [
+#       0,
+#       2
+#     ],
+#     "subject_type": "子宫",
+#     "object": "在正常范围",
+#     "object_idx": [
+#       4,
+#       9
+#     ],
+#     "object_type": "子宫_大小",
+#     "predicate": "子宫_大小"
+#   },
+#   {
+#     "subject": "子宫",
+#     "subject_idx": [
+#       0,
+#       2
+#     ],
+#     "subject_type": "子宫",
+#     "object": "规则",
+#     "object_idx": [
+#       12,
+#       14
+#     ],
+#     "object_type": "子宫_边缘",
+#     "predicate": "子宫_边缘"
+#   },
+#   {
+#     "subject": "子宫内膜",
+#     "subject_idx": [
+#       30,
+#       34
+#     ],
+#     "subject_type": "子宫内膜",
+#     "object": "12mm",
+#     "object_idx": [
+#       37,
+#       41
+#     ],
+#     "object_type": "厚度",
+#     "predicate": "子宫内膜_厚度"
+#   },
+#   {
+#     "subject": "CDFI",
+#     "subject_idx": [
+#       79,
+#       83
+#     ],
+#     "subject_type": "CDFI",
+#     "object": "未见异常血流信号",
+#     "object_idx": [
+#       84,
+#       92
+#     ],
+#     "object_type": "CDFI表现",
+#     "predicate": "CDFI表现"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       70,
+#       72
+#     ],
+#     "subject_type": "附件",
+#     "object": "右",
+#     "object_idx": [
+#       68,
+#       69
+#     ],
+#     "object_type": "侧别",
+#     "predicate": "附件_侧别"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       44,
+#       46
+#     ],
+#     "subject_type": "附件",
+#     "object": "左",
+#     "object_idx": [
+#       42,
+#       43
+#     ],
+#     "object_type": "侧别",
+#     "predicate": "附件_侧别"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       70,
+#       72
+#     ],
+#     "subject_type": "附件",
+#     "object": "未见明显异常",
+#     "object_idx": [
+#       72,
+#       78
+#     ],
+#     "object_type": "附件_回声表现",
+#     "predicate": "附件_回声表现"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       44,
+#       46
+#     ],
+#     "subject_type": "附件",
+#     "object": "25×34mm",
+#     "object_idx": [
+#       47,
+#       54
+#     ],
+#     "object_type": "回声大小",
+#     "predicate": "附件_回声大小"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       44,
+#       46
+#     ],
+#     "subject_type": "附件",
+#     "object": "无回声",
+#     "object_idx": [
+#       54,
+#       57
+#     ],
+#     "object_type": "回声强度",
+#     "predicate": "附件_回声强度"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       44,
+#       46
+#     ],
+#     "subject_type": "附件",
+#     "object": "清晰",
+#     "object_idx": [
+#       60,
+#       62
+#     ],
+#     "object_type": "边界表现",
+#     "predicate": "附件_边界表现"
+#   },
+#   {
+#     "subject": "附件",
+#     "subject_idx": [
+#       44,
+#       46
+#     ],
+#     "subject_type": "附件",
+#     "object": "平滑",
+#     "object_idx": [
+#       65,
+#       67
+#     ],
+#     "object_type": "囊壁表现",
+#     "predicate": "附件_囊壁表现"
+#   },
+#   {
+#     "subject": "子宫肌层",
+#     "subject_idx": [
+#       15,
+#       19
+#     ],
+#     "subject_type": "子宫肌层",
+#     "object": "均匀",
+#     "object_idx": [
+#       21,
+#       23
+#     ],
+#     "object_type": "边界表现",
+#     "predicate": "子宫肌层_边界表现"
+#   }
+# ]
+# *******************************
+
+"""
+子宫大小在正常范围,边缘规则,子宫肌层回声均匀;宫腔线居中,子宫内膜厚度为12mm。左侧附件见25×34mm无回声,边界清晰,囊壁平滑。右侧附件未见明显异常。CDFI:未见异常血流信号。
+
+{'subject': '子宫', 'subject_idx': [0, 2], 'object': '在正常范围', 'object_idx': [4, 9]}
+{'subject': '子宫', 'subject_idx': [0, 2], 'object': '规则', 'object_idx': [12, 14]}
+{'subject': '子宫内膜', 'subject_idx': [30, 34], 'object': '12mm', 'object_idx': [37, 41]}
+{'subject': 'CDFI', 'subject_idx': [79, 83], 'object': '未见异常血流信号', 'object_idx': [84, 92]}
+{'subject': '附件', 'subject_idx': [70, 72], 'object': '右', 'object_idx': [68, 69]}
+{'subject': '附件', 'subject_idx': [44, 46], 'object': '左', 'object_idx': [42, 43]}
+{'subject': '附件', 'subject_idx': [70, 72], 'object': '未见明显异常', 'object_idx': [72, 78]}
+{'subject': '附件', 'subject_idx': [44, 46], 'object': '25×34mm', 'object_idx': [47, 54]}
+{'subject': '附件', 'subject_idx': [44, 46], 'object': '无回声', 'object_idx': [54, 57]}
+{'subject': '附件', 'subject_idx': [44, 46], 'object': '清晰', 'object_idx': [60, 62]}
+{'subject': '附件', 'subject_idx': [44, 46], 'object': '平滑', 'object_idx': [65, 67]}
+{'subject': '子宫肌层', 'subject_idx': [15, 19], 'object': '均匀', 'object_idx': [21, 23]}
+
+Process finished with exit code 0
+"""
